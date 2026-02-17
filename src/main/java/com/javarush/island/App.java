2cb6fec3493d0342751d67a3b4aceddb9b3716d5
Elena Moskvina <e.moskvina.jj@gmail.com>
2026-02-17 20:50:23 +0500

    Init

--- src/main/java/com/javarush/island/App.java
+++ src/main/java/com/javarush/island/App.java
@@ -1,7 +1,7 @@
 package com.javarush.island;
 
 public class App {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello World");
     }
 }
