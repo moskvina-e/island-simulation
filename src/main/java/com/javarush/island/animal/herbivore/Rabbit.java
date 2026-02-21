4bc57f9c8c57c14b3cd6c8956d15f41509bd2359
Elena Moskvina <e.moskvina.jj@gmail.com>
2026-02-21 21:34:47 +0500

    upd

--- src/main/java/com/javarush/island/animal/herbivore/Rabbit.java
+++ src/main/java/com/javarush/island/animal/herbivore/Rabbit.java
@@ -1,33 +1,19 @@
 package com.javarush.island.animal.herbivore;
 
-import com.javarush.island.animal.Animal;
 import com.javarush.island.model.Island;
 import com.javarush.island.model.Location;
-import com.javarush.island.model.Plant;
-import lombok.extern.slf4j.Slf4j;
 
 /*
  * Rabbit травоядное
  */
-@Slf4j
-public class Rabbit extends Animal {
+
+public class Rabbit extends Herbivore {
 
     private static final double WEIGHT = 2;
     private static final double MAX_SATIETY = 0.45;
 
     public Rabbit() {
         super(WEIGHT, MAX_SATIETY);
-    }
-
-    @Override
-    public void eat(Location location) {
-        if(!this.isAlive())
-            return;
-        Plant plant = location.removePlant();
-        if (plant != null) {
-            currentSatiety = Math.min(maxSatiety, currentSatiety + plant.getWeight());
-            log.debug("Кролик съел растение");
-        }
     }
 
     @Override
