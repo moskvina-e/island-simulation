0d50c3d8b78628aa07bc9d5b581cf319265d8c5f
Elena Moskvina <e.moskvina.jj@gmail.com>
2026-02-21 21:38:57 +0500

    feat add Herbivore class

---
+++ src/main/java/com/javarush/island/animal/herbivore/Herbivore.java
@@ -0,0 +1,25 @@
+package com.javarush.island.animal.herbivore;
+
+import com.javarush.island.animal.Animal;
+import com.javarush.island.model.Location;
+import com.javarush.island.model.Plant;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+public abstract class Herbivore extends Animal {
+
+    public Herbivore(double weight, double maxSatiety) {
+        super(weight, maxSatiety);
+    }
+
+    public void eat(Location location) {
+        if(!this.isAlive())
+            return;
+        Plant plant = location.removePlant();
+        if (plant != null) {
+            currentSatiety = Math.min(maxSatiety, currentSatiety + plant.getWeight());
+            log.debug("{} съел растение",this.getClass().getSimpleName());
+        }
+    }
+}
