3eb8ee919724c4f7fdd452059565436c2a341417
Elena Moskvina <e.moskvina.jj@gmail.com>
2026-02-21 21:38:40 +0500

    upd fill Deer class

--- src/main/java/com/javarush/island/animal/herbivore/Deer.java
+++ src/main/java/com/javarush/island/animal/herbivore/Deer.java
@@ -1,4 +1,28 @@
 package com.javarush.island.animal.herbivore;
 
-public class Deer {
+/*
+ * Deer травоядное
+ */
+
+import com.javarush.island.model.Island;
+import com.javarush.island.model.Location;
+
+public class Deer extends Herbivore {
+
+    private static final double WEIGHT = 300;
+    private static final double MAX_SATIETY = 50;
+
+    public Deer() {
+        super(WEIGHT, MAX_SATIETY);
+    }
+
+    @Override
+    public void move(Island island, int currentX, int currentY) {
+        //todo Пока заглушка
+    }
+
+    @Override
+    public void reproduce(Location location) {
+        //todo Пока заглушка
+    }
 }
