1d16b1f38157622170216ca1987a262eb4d1980a
Elena Moskvina <e.moskvina.jj@gmail.com>
2026-03-09 14:53:15 +0500

    upd change SimulationConfig constructor from Builder to properties file

--- src/main/java/com/javarush/island/App.java
+++ src/main/java/com/javarush/island/App.java
@@ -5,53 +5,14 @@ import com.javarush.island.animal.omnivorous.*;
 import com.javarush.island.animal.predator.*;
 import com.javarush.island.config.SimulationConfig;
 import com.javarush.island.simulation.MultithreadedSimulation;
-import com.javarush.island.simulation.SimpleSimulation;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.Map;
-import java.util.concurrent.ThreadLocalRandom;
 
 @Slf4j
 public class App {
 
-    //private static final int SIMPLE_SIMULATION_TICKS = 10;
-
     public static void main(String[] args) {
-        int height = 5;
-        int width = 5;
-        int square = height * width;
-        SimulationConfig config = SimulationConfig.builder()
-                .islandHeight(height)
-                .islandWidth(width)
-                .initialAnimals(Map.ofEntries(
-                        Map.entry(Buffalo.class, ThreadLocalRandom.current().nextInt(square, square * Buffalo.MAX_NUMBER_OF_ANIMALS_PER_CELL)),
-                        Map.entry(Caterpillar.class, ThreadLocalRandom.current().nextInt(square, square * Caterpillar.MAX_NUMBER_OF_ANIMALS_PER_CELL)),
-                        Map.entry(Goat.class, ThreadLocalRandom.current().nextInt(square, square * Goat.MAX_NUMBER_OF_ANIMALS_PER_CELL)),
-                        Map.entry(Horse.class, ThreadLocalRandom.current().nextInt(square, square * Horse.MAX_NUMBER_OF_ANIMALS_PER_CELL)),
-                        Map.entry(Sheep.class, ThreadLocalRandom.current().nextInt(square, square * Sheep.MAX_NUMBER_OF_ANIMALS_PER_CELL)),
-                        Map.entry(Boar.class, ThreadLocalRandom.current().nextInt(square, square * Boar.MAX_NUMBER_OF_ANIMALS_PER_CELL)),
-                        Map.entry(Duck.class, ThreadLocalRandom.current().nextInt(square, square * Duck.MAX_NUMBER_OF_ANIMALS_PER_CELL)),
-                        Map.entry(Mouse.class, ThreadLocalRandom.current().nextInt(square, square * Mouse.MAX_NUMBER_OF_ANIMALS_PER_CELL)),
-                        Map.entry(Bear.class, ThreadLocalRandom.current().nextInt(square, square * Bear.MAX_NUMBER_OF_ANIMALS_PER_CELL)),
-                        Map.entry(Boa.class, ThreadLocalRandom.current().nextInt(square, square * Boa.MAX_NUMBER_OF_ANIMALS_PER_CELL)),
-                        Map.entry(Eagle.class, ThreadLocalRandom.current().nextInt(square, square * Eagle.MAX_NUMBER_OF_ANIMALS_PER_CELL)),
-                        Map.entry(Fox.class, ThreadLocalRandom.current().nextInt(square, square * Fox.MAX_NUMBER_OF_ANIMALS_PER_CELL)),
-                        Map.entry(Wolf.class, ThreadLocalRandom.current().nextInt(square, square * Wolf.MAX_NUMBER_OF_ANIMALS_PER_CELL)),
-                        Map.entry(Rabbit.class, ThreadLocalRandom.current().nextInt(square, square * Rabbit.MAX_NUMBER_OF_ANIMALS_PER_CELL)),
-                        Map.entry(Deer.class, ThreadLocalRandom.current().nextInt(square, square * Deer.MAX_NUMBER_OF_ANIMALS_PER_CELL))))
-                .plantsPerSell(5)
-                .tickDurationMs(1000)
-                .build();
 
-        // Однопоточная симуляция
-        /*SimpleSimulation simpleSimulation = new SimpleSimulation(config);
-        simpleSimulation.initialize();
-        try {
-            simpleSimulation.run(SIMPLE_SIMULATION_TICKS);
-        } catch (InterruptedException e) {
-            log.error("Simulation interrupted");
-            throw new RuntimeException(e);
-        }*/
+        SimulationConfig config = new SimulationConfig();
 
         // Многопоточная симуляция
         MultithreadedSimulation multithreadedSimulation = new MultithreadedSimulation(config);
@@ -68,5 +29,16 @@ public class App {
             throw new RuntimeException(e);
         }
         multithreadedSimulation.stop();
+
+        // Однопоточная симуляция
+        /* private static final int SIMPLE_SIMULATION_TICKS = 10;
+        SimpleSimulation simpleSimulation = new SimpleSimulation(config);
+        simpleSimulation.initialize();
+        try {
+            simpleSimulation.run(SIMPLE_SIMULATION_TICKS);
+        } catch (InterruptedException e) {
+            log.error("Simulation interrupted");
+            throw new RuntimeException(e);
+        }*/
     }
 }
--- src/main/java/com/javarush/island/config/SimulationConfig.java
+++ src/main/java/com/javarush/island/config/SimulationConfig.java
@@ -1,24 +1,105 @@
 package com.javarush.island.config;
 
 import com.javarush.island.animal.Animal;
-import lombok.Builder;
+import com.javarush.island.animal.herbivore.*;
+import com.javarush.island.animal.omnivorous.*;
+import com.javarush.island.animal.predator.*;
 import lombok.Data;
-import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Data
-@Builder
+@Slf4j
 public class SimulationConfig {
 
     //Размеры острова
     private int islandWidth;
     private int islandHeight;
+    private int square;
 
     //Популяции животных
     private Map<Class<? extends Animal>, Integer> initialAnimals;
 
     //Кол-во растений, которые будут добавляться за 1 такт в каждую клетку
-    private int plantsPerSell;
+    private int plantsPerCell;
 
     //Дюрация в мс
     private long tickDurationMs;
+
+    private Properties properties;
+
+    public SimulationConfig() {
+        this.properties = new Properties();
+        if(loadPropertiesFile())
+            extractConfigValues();
+        square = islandWidth * islandHeight;
+        initialAnimals = initializeAnimals();
+    }
+
+    private boolean loadPropertiesFile() {
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("application.properties")) {
+            properties.load(input);
+            return true;
+        } catch (NullPointerException | IOException e) {
+            log.error("Ошибка загрузки файла: " + e.getMessage());
+            setDefaultValues();
+            return false;
+        }
+    }
+
+    private void extractConfigValues() {
+        String heightStr = properties.getProperty("island.height", "5");
+        String widthStr = properties.getProperty("island.width", "5");
+        String plantsStr = properties.getProperty("island.plants.per.cell", "5");
+        String tickStr = properties.getProperty("simulation.tick.duration.ms", "1000");
+
+        try {
+            islandHeight = Integer.parseInt(heightStr.trim());
+            islandWidth = Integer.parseInt(widthStr.trim());
+            plantsPerCell = Integer.parseInt(plantsStr.trim());
+            tickDurationMs = Long.parseLong(tickStr.trim());
+        }  catch (NumberFormatException e) {
+            log.error("Ошибка преобразования чисел, использую значения по умолчанию");
+            setDefaultValues();
+        }
+    }
+
+    private void setDefaultValues() {
+        islandWidth = 5;
+        islandHeight = 5;
+        plantsPerCell = 5;
+        tickDurationMs = 1000;
+    }
+
+    private Map<Class<? extends Animal>, Integer> initializeAnimals() {
+        Map<Class<? extends Animal>, Integer> animals = new HashMap<>();
+
+        // Словарь с максимальными кол-вами животного на 1 клетку для каждого вида животного
+        Map<Class<? extends Animal>, Integer> maxValues = new HashMap<>();
+        maxValues.put(Buffalo.class, Buffalo.MAX_NUMBER_OF_ANIMALS_PER_CELL);
+        maxValues.put(Caterpillar.class, Caterpillar.MAX_NUMBER_OF_ANIMALS_PER_CELL);
+        maxValues.put(Goat.class, Goat.MAX_NUMBER_OF_ANIMALS_PER_CELL);
+        maxValues.put(Horse.class, Horse.MAX_NUMBER_OF_ANIMALS_PER_CELL);
+        maxValues.put(Sheep.class, Sheep.MAX_NUMBER_OF_ANIMALS_PER_CELL);
+        maxValues.put(Boar.class, Boar.MAX_NUMBER_OF_ANIMALS_PER_CELL);
+        maxValues.put(Duck.class, Duck.MAX_NUMBER_OF_ANIMALS_PER_CELL);
+        maxValues.put(Mouse.class, Mouse.MAX_NUMBER_OF_ANIMALS_PER_CELL);
+        maxValues.put(Bear.class, Bear.MAX_NUMBER_OF_ANIMALS_PER_CELL);
+        maxValues.put(Boa.class, Boa.MAX_NUMBER_OF_ANIMALS_PER_CELL);
+        maxValues.put(Eagle.class, Eagle.MAX_NUMBER_OF_ANIMALS_PER_CELL);
+        maxValues.put(Fox.class, Fox.MAX_NUMBER_OF_ANIMALS_PER_CELL);
+        maxValues.put(Wolf.class, Wolf.MAX_NUMBER_OF_ANIMALS_PER_CELL);
+        maxValues.put(Rabbit.class, Rabbit.MAX_NUMBER_OF_ANIMALS_PER_CELL);
+        maxValues.put(Deer.class, Deer.MAX_NUMBER_OF_ANIMALS_PER_CELL);
+
+        for (Map.Entry<Class<? extends Animal>, Integer> entry : maxValues.entrySet()) {
+            int count = ThreadLocalRandom.current().nextInt(square, square * entry.getValue());
+            animals.put(entry.getKey(), count);
+        }
+        return animals;
+    }
 }
